23340817176e2ae822d38a5bfb6926ffc17c033d
emc <micha3l_255@yahoo.com>
2020-01-09 10:44:12 -0600

    test for host

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
 const cacheName = "v1";
 
-const cachedAssets = ["index.html", "restaurant.html", "./css/*.css", "./data/*.*", "./js/*.js", "./img/*.*"];
+const cachedAssets = [
+  "index.html",
+  "restaurant.html",
+  "./css/styles.css",
+  "./data/restaurants.json",
+  "./js/dbhelper.js",
+  "./js/main.js",
+  "./js/restaurant_info.js",
+  "./img/1.jpg",
+  "./img/2.jpg",
+  "./img/3.jpg",
+  "./img/4.jpg",
+  "./img/5.jpg",
+  "./img/6.jpg",
+  "./img/7.jpg",
+  "./img/8.jpg",
+  "./img/9.jpg",
+  "./img/10.jpg"
+];
 
 self.addEventListener("install", e => {
   console.log("installed");
